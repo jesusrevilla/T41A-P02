a061bdbac7a7df36e0771a4cb2240224028284c7
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-09-03 19:41:57 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -1,9 +1,27 @@
 -- ==============================================
--- Mostrar tablas
+-- Mostrar faltas
 -- ==============================================
-SELECT * FROM Grupos;
-SELECT * FROM Profesor;
-SELECT * FROM Curso;
-SELECT * FROM Alumnos;
-SELECT * FROM Inscripcion;
-SELECT * FROM Asistencia;
+SELECT
+    c.nombre_curso AS "Materia",
+    g.codigo AS "Grupo",
+    a.nombre || ' ' || a.apellidos AS "Alumno",
+    a.matricula AS "Matrícula",
+    COUNT(*) FILTER (WHERE asis.presente = FALSE) AS "Faltas"
+FROM
+    Asistencia asis
+JOIN
+    Inscripcion i ON asis.id_inscripcion = i.id_inscripcion
+JOIN
+    Alumnos a ON i.id_alumno = a.id_alumno
+JOIN
+    Curso c ON i.id_curso = c.id_curso
+JOIN
+    Grupos g ON a.id_grupo = g.id_grupo
+GROUP BY
+    c.nombre_curso,
+    g.codigo,
+    a.id_alumno
+ORDER BY
+    c.nombre_curso,
+    g.codigo,
+    a.apellidos;
