35b65586a17ad414cc525b14faffe9ae69c6a6af
aldogg10 <179862@upslp.edu.mx>
2025-09-03 23:30:13 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -1,6 +1,6 @@
 SELECT g.periodo, g.id_curso, g.nombre_curso,
     COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
-FROM Asistencia a
-JOIN Grupo g ON a.id_curso = g.id_curso AND a.periodo = g.periodo
+FROM asistencia a
+JOIN grupo g ON a.id_curso = g.id_curso AND a.periodo = g.periodo
 GROUP BY g.periodo, g.id_curso, g.nombre_curso
 ORDER BY g.id_curso;
