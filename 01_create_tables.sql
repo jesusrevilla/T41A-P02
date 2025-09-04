cccb14e80f3620f7f0858aa1bbb01cea45eb887a
aldogg10 <179862@upslp.edu.mx>
2025-09-03 22:51:40 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,8 +1,7 @@
 CREATE TABLE Estudiante (
   id_estudiante INTEGER PRIMARY KEY,
   nombre TEXT NOT NULL,
-  apellido TEXT NOT NULL,
-  fecha_nacimiento DATE NOT NULL
+  apellido TEXT NOT NULL
 );
 
 CREATE TABLE Maestros (
@@ -13,21 +12,28 @@ CREATE TABLE Maestros (
 );
 
 CREATE TABLE Grupo (
-  id_curso INTEGER PRIMARY KEY,
+  id_curso varchar(15),
+  periodo varchar(15),
   id_profesor INT REFERENCES Maestros(id_profesor) NOT NULL,
   nombre_curso TEXT NOT NULL,
-  creditos TEXT NOT NULL
+  creditos TEXT NOT NULL,
+  PRIMARY KEY (id_curso, periodo)
 );
+
 
 CREATE TABLE Inscripcion (
   id_estudiante INT REFERENCES Estudiante(id_estudiante) NOT NULL,
-  id_curso INT REFERENCES Grupo(id_curso) NOT NULL,
-  fecha_inscripcion DATE NOT NULL
+  id_curso varchar(15),
+  periodo varchar(15),
+  fecha_inscripcion DATE NOT NULL,
+  FOREIGN KEY (id_curso, periodo) REFERENCES Grupo(id_curso, periodo)
 );
 
-CREATE TABLE Asistencia(
-  id_matricula INT REFERENCES Estudiante(id_estudiante) NOT NULL,
-  id_curso INTEGER REFERENCES Grupo(id_curso) NOT NULL, 
+CREATE TABLE Asistencia (
+  id_estudiante INT REFERENCES Estudiante(id_estudiante) NOT NULL,
+  id_curso varchar(15) NOT NULL,
+  periodo varchar(15) NOT NULL,
   fecha_hora TIMESTAMP,
-  presente boolean
+  presente boolean,
+  FOREIGN KEY (id_curso, periodo) REFERENCES Grupo(id_curso, periodo)
 );
