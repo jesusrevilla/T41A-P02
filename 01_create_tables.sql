785911fda0e37911ed6b032fe81bd6b94fac461a
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-09-03 19:35:53 -0600

    feature/182239

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,51 +1,38 @@
+CREATE TABLE estudiante(
+  matricula VARCHAR(20) PRIMARY KEY,
+  nombre VARCHAR(150) NOT NULL
+);
 
-    CREATE TABLE alumnos (
-        matricula VARCHAR(20) PRIMARY KEY,
-        nombre VARCHAR(100) NOT NULL
-    );
+CREATE TABLE profesor(
+  profesor_id SERIAL PRIMARY KEY,
+  nombre VARCHAR(150) NOT NULL
+);
 
-    CREATE TABLE maestros (
-        id_maestro SERIAL PRIMARY KEY,
-        nombre VARCHAR(100) NOT NULL
-    );
+CREATE TABLE grupo(
+  periodo VARCHAR(50) NOT NULL,
+  seccion VARCHAR(6) NOT NULL,
+  nombre_grupo VARCHAR(50) NOT NULL,
+  id_profesor SERIAL REFERENCES profesor(profesor_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (periodo, seccion)
+);
 
-    CREATE TABLE grupos (
-        periodo VARCHAR(10) NOT NULL,
-        seccion VARCHAR(10) NOT NULL,
-        nombre_grupo VARCHAR(50) NOT NULL,
-        id_maestro INTEGER NOT NULL,
-        PRIMARY KEY (periodo, seccion),
-        FOREIGN KEY (id_maestro) REFERENCES maestros(id_maestro)
-            ON DELETE RESTRICT
-            ON UPDATE CASCADE
-    );
+CREATE TABLE inscripcion(
+  matricula VARCHAR(20) NOT NULL,
+  periodo VARCHAR(50) NOT NULL,
+  seccion VARCHAR(6) NOT NULL,
+  fecha_inscripcion DATE NOT NULL,
+  FOREIGN KEY (matricula) REFERENCES estudiante(matricula) ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (periodo, seccion) REFERENCES grupo(periodo, seccion) ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (matricula, periodo, seccion)
+);
 
-    CREATE TABLE inscripciones (
-        matricula VARCHAR(20) NOT NULL,
-        periodo VARCHAR(10) NOT NULL,
-        seccion VARCHAR(10) NOT NULL,
-        fecha_inscripcion DATE NOT NULL,
-        FOREIGN KEY (matricula) REFERENCES alumnos(matricula)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE,
-        FOREIGN KEY (periodo, seccion) REFERENCES grupos(periodo, seccion)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE,
-        PRIMARY KEY (matricula, periodo, seccion)
-    );
-
-    CREATE TABLE asistencia (
-        matricula VARCHAR(20) NOT NULL,
-        periodo VARCHAR(10) NOT NULL,
-        seccion VARCHAR(10) NOT NULL,
-        fecha_hora TIMESTAMP NOT NULL,
-        presente BOOLEAN NOT NULL,
-        FOREIGN KEY (matricula) REFERENCES alumnos(matricula)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE,
-        FOREIGN KEY (periodo, seccion) REFERENCES grupos(periodo, seccion)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE,
-        PRIMARY KEY (matricula, periodo, seccion, fecha_hora)
-    );
-    
+CREATE TABLE asistencia (
+    matricula VARCHAR(20) NOT NULL,
+    periodo VARCHAR(10) NOT NULL,
+    seccion VARCHAR(10) NOT NULL,
+    fecha_hora TIMESTAMP NOT NULL,
+    presente BOOLEAN NOT NULL,
+    FOREIGN KEY (matricula) REFERENCES estudiante(matricula) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (periodo, seccion) REFERENCES grupo(periodo, seccion) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (matricula, periodo, seccion, fecha_hora)
+);
