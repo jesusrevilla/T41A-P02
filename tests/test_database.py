a0ea0fecfc44084fc7abf86b709ae58014ef51cf
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-09-03 19:38:26 -0600

    feature/182239

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,27 +13,27 @@ def db_connection():
     yield conn
     conn.close()
 
-def test_alumnos_insertados(db_connection):
+def test_estudiantes_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM alumnos;")
+        cur.execute("SELECT COUNT(*) FROM estudiante;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_maestros_insertados(db_connection):
+def test_profesores_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM maestros;")
+        cur.execute("SELECT COUNT(*) FROM profesor;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_grupos_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM grupos;")
+        cur.execute("SELECT COUNT(*) FROM grupo;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_inscripciones_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM inscripciones;")
+        cur.execute("SELECT COUNT(*) FROM inscripcion;")
         count = cur.fetchone()[0]
         assert count == 10  
 
@@ -47,11 +47,11 @@ def test_structure(db_connection):
     sql = ''' 
             SELECT tablename FROM pg_tables 
             WHERE tablename 
-            IN ('alumnos', 'maestros', 'grupos',
-            'inscripciones', 'asistencia');
+            IN ('estudiante', 'profesor', 'grupos',
+            'inscripcion', 'asistencia');
           '''
-    expected_tables = {'alumnos', 'maestros',
-                       'grupos', 'inscripciones',
+    expected_tables = {'estudiante', 'profesor',
+                       'grupo', 'inscripcion',
                        'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
