4b87f01e2ae9d03ebb2b2cf7ffd5a115378ca83d
Fano-Rdz <178584@upslp.edu.mx>
2025-09-03 19:27:11 -0600

    Database

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,25 +13,25 @@ def db_connection():
     yield conn
     conn.close()
 
-def test_alumnos_insertados(db_connection):
+def test_alumno_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM alumnos;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_maestros_insertados(db_connection):
+def test_maestro_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM maestros;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_grupos_insertados(db_connection):
+def test_grupo_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM grupos;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_inscripciones_insertados(db_connection):
+def test_inscripcion_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM inscripciones;")
         count = cur.fetchone()[0]
