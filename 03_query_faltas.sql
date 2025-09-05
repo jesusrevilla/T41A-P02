ebd6fdda66ab1d1fd25f85dfe751384c5a8fceaf
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-09-04 20:45:14 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -2,21 +2,12 @@
 -- Mostrar faltas
 -- ==============================================
  SELECT
-    g.codigo AS grupo,
-    c.nombre_curso AS nombre_materia,
-    COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
-FROM
-    Asistencia a
-JOIN
-    Inscripcion i ON a.id_inscripcion = i.id_inscripcion
-JOIN
-    Curso c ON i.id_curso = c.id_curso
-JOIN
-    Alumnos al ON i.id_alumno = al.id_alumno
-JOIN
-    Grupos g ON al.id_grupo = g.id_grupo
-GROUP BY
-    g.codigo,
-    c.nombre_curso
-ORDER BY
-    g.codigo;
+  g.codigo AS grupo,
+  COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
+FROM asistencia a
+JOIN inscripcion i   ON a.id_inscripcion = i.id_inscripcion
+JOIN alumnos al      ON i.id_alumno = al.id_alumno
+LEFT JOIN grupos g   ON al.id_grupo = g.id_grupo
+GROUP BY g.codigo
+ORDER BY g.codigo;
+
